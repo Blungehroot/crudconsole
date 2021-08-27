f19e4dfe7885edf9ce8cef57f0b30aa4bbf1b08a
Yevgeniy Shaydur <yevgeniy.shaydur@kyivstar.net>
2021-08-27 17:33:47 +0300

    impl repository label

--- src/main/java/com/crudconsole/app/controller/LabelController.java
+++ src/main/java/com/crudconsole/app/controller/LabelController.java
@@ -1,5 +1,35 @@
 package com.crudconsole.app.controller;
 
-public class LabelController {
+import com.crudconsole.app.model.Label;
+import com.crudconsole.app.repository.LabelRepositoryImpl;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+public class LabelController {
+    LabelRepositoryImpl labelRepository;
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    public LabelController(LabelRepositoryImpl labelRepository) {
+        this.labelRepository = labelRepository;
+    }
+
+    public String getAll() {
+        return gson.toJson(labelRepository.getAll());
+    }
+
+    public String getById(Long id) {
+        return gson.toJson(labelRepository.getById(id));
+    }
+
+    public void create(Label label) {
+        labelRepository.save(label);
+    }
+
+    public void update(Label label) {
+        labelRepository.update(label);
+    }
+
+    public void delete(Long id) {
+        labelRepository.delete(id);
+    }
 }
